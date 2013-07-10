6ecfcecb1ed04251f87f5f4dc22be8270dea7f10
pulse-akshay <tandon.akshay@gmail.com>
2013-07-10 17:18:26 +0000

    web.js

--- web.js
+++ web.js
@@ -1,9 +1,14 @@
 var express = require('express');
+var fs = require('fs');
+
+//read the fild and get the strings
+var buffer = fs.readFileSync(./index.html);
+var text = buffer.toString("utf-8");
 
 var app = express.createServer(express.logger());
 
 app.get('/', function(request, response) {
-  response.send('Hello World2!');
+  response.send(text);
 });
 
 var port = process.env.PORT || 5000;
